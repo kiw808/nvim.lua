cb148258ae719bde50d530367e64e6fe5e2c12ee
Pierre Muller <pierremanu.muller@gmail.com>
2021-12-30 00:21:21 +0100

    feat(lualine): square separators

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,7 +1,10 @@
 require'lualine'.setup {
     options = {
         theme = 'auto',
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-  },
+        -- component_separators = { left = '', right = ''},
+        -- section_separators = { left = '', right = ''},
+        component_separators = '', 
+        section_separators = '', 
+    },
+    extensions = {'nvim-tree'},
 }
