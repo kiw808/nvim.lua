49292f7c4102d15a725ff25ccfbc489b2f5d430a
Pierre Muller <pierremanu.muller@gmail.com>
2021-12-30 17:59:54 +0100

    feat(lualine): add language server section

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,10 +1,58 @@
-require'lualine'.setup {
+local lualine = require'lualine'
+
+
+local config = {
     options = {
+        icons_enabled = true,
         theme = 'auto',
-        -- component_separators = { left = '', right = ''},
-        -- section_separators = { left = '', right = ''},
-        component_separators = '', 
-        section_separators = '', 
+        component_separators = { left = '', right = '|'},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {},
+        always_divide_middle = true,
     },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
     extensions = {'nvim-tree'},
 }
+
+-- Inserts a component in lualine_x ot right section
+local function ins_right(component)
+    table.insert(config.sections.lualine_x, component)
+end
+
+ins_right {
+    -- Lsp server name .
+    function()
+        local msg = 'No LSP'
+        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+        local clients = vim.lsp.get_active_clients()
+        if next(clients) == nil then
+            return msg
+        end
+        for _, client in ipairs(clients) do
+            local filetypes = client.config.filetypes
+            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                return client.name
+            end
+        end
+        return msg
+    end,
+    icon = '',
+}
+
+lualine.setup(config)
