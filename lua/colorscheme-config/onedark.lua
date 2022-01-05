c4cd5cb893ee68a9577b350cd5da353a57cc18fc
Pierre Muller <pierremanu.muller@gmail.com>
2022-01-05 16:50:22 +0100

    feat(onedark): default theme

--- lua/colorscheme-config/onedark.lua
+++ lua/colorscheme-config/onedark.lua
@@ -1,5 +1,5 @@
 -- vim.g.onedark_style = 'dark'
 -- vim.g.onedark_style = 'darker'
 -- vim.g.onedark_style = 'cool'
-vim.g.onedark_style = 'deep'
+-- vim.g.onedark_style = 'deep'
 require('onedark').setup()
